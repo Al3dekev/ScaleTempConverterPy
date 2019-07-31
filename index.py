d6c93b366a802c8a12bc2a417d34d3d16e6ebe4d
Al3dekev <quentin1998@me.com>
2019-07-31 19:22:05 +0200

    Conversion Loop updated

--- index.py
+++ index.py
@@ -7,7 +7,7 @@ from UserCallConverter import *
 from UniversalConverter import UniversalConverter
 from ScaleUniversalDatas import *
 
-#scaleNumberInProg = 2
+# scaleNumberInProg = 2
 
 Beg = Beginning()
 Beg.welcome()
@@ -19,34 +19,37 @@ UCC.selectNumToConvert()
 UCC.selectScaleFrom()
 UCC.selectScaleTo()
 
-UC = UniversalConverter()
+UC = UniversalConverter(UCC.getSelNum(), UCC.getSelScaleF(), UCC.getselScaleT())
 
 for el in scaleList:
-    # if el.get("ScaleSymbol") == UCC.getSelScaleF():
-    #     for ele in scaleList:
-    #         if ele.get("ScaleSymbol") == UCC.getselScaleT():
 
     # From
-    if el.get("ScaleSymbol") == "C":
+    if el.get("ScaleSymbol") == "C" and el.get("ScaleSymbol") == UCC.getSelScaleF():
         for ele in scaleList:
             # To
-            if ele.get("ScaleSymbol") == "F":
+            if ele.get("ScaleSymbol") == "F" and ele.get("ScaleSymbol") == UCC.getselScaleT():
                 UC.convertCtoF()
-            if ele.get("ScaleSymbol") == "K":
+            if ele.get("ScaleSymbol") == "K" and ele.get("ScaleSymbol") == UCC.getselScaleT():
                 UC.convertCtoK()
-    if el.get("ScaleSymbol") == "F":
+    if el.get("ScaleSymbol") == "F" and el.get("ScaleSymbol") == UCC.getSelScaleF():
         for ele in scaleList:
             # To
-            if ele.get("ScaleSymbol") == "C":
+            if ele.get("ScaleSymbol") == "C" and ele.get("ScaleSymbol") == UCC.getselScaleT():
                 UC.convertFtoC()
-            if ele.get("ScaleSymbol") == "K":
+            if ele.get("ScaleSymbol") == "K" and ele.get("ScaleSymbol") == UCC.getselScaleT():
                 UC.convertFtoK()
-    if el.get("ScaleSymbol") == "K":
+    if el.get("ScaleSymbol") == "K" and el.get("ScaleSymbol") == UCC.getSelScaleF():
         for ele in scaleList:
             # To
-            if ele.get("ScaleSymbol") == "C":
+            if ele.get("ScaleSymbol") == "C" and ele.get("ScaleSymbol") == UCC.getselScaleT():
                 UC.convertKtoC()
-            if ele.get("ScaleSymbol") == "F":
+            if ele.get("ScaleSymbol") == "F" and ele.get("ScaleSymbol") == UCC.getselScaleT():
                 UC.convertKtoF()
 
 # End for loop
+
+while 1:
+    lett = input("Press q to exit program")
+    if lett == "q":
+        exit()
+        break
