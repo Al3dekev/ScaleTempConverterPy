f1c93a184a9d8c469e21ac2649f1fa07d3f967ba
Al3dekev <quentin1998@me.com>
2019-07-31 19:23:35 +0200

    UniversalConverter: Txt added, methods updated

--- UniversalConverter.py
+++ UniversalConverter.py
@@ -3,37 +3,37 @@
 class UniversalConverter:
 
     def __init__(self, number, FromScale, ToScale):
-        self.number = number
+        self.number = int(number)
         self.FromScale = FromScale
         self.ToScale = ToScale
 
 
     # From C
     def convertCtoF(self):
-        r = (self.FromScale*9/5) + 32
-        return r
+        r = (self.number * 9 / 5) + 32
+        return print(str(self.number)+" °"+self.FromScale+" have been converted into "+str(r)+" °"+self.ToScale)
 
     def convertCtoK(self):
-        r = self.FromScale + 273.15
-        return r
+        r = self.number + 273.15
+        return print(str(self.number)+" °"+self.FromScale+" have been converted into "+str(r)+" °"+self.ToScale)
     # End - From C
 
     # From F
     def convertFtoC(self):
-        r = (self.FromScale-32)*5/9
-        return r
+        r = (self.number-32)*5/9
+        return print(str(self.number)+" °"+self.FromScale+" have been converted into "+str(r)+" °"+self.ToScale)
 
     def convertFtoK(self):
-        r = (self.FromScale-32)*5/9 + 273.15
-        return r
+        r = (self.number-32)*5/9 + 273.15
+        return print(str(self.number)+" °"+self.FromScale+" have been converted into "+str(r)+" °"+self.ToScale)
     # End - From F
 
     # From K
     def convertKtoC(self):
-        r = self.FromScale - 273.15
-        return r
+        r = self.number - 273.15
+        return print(str(self.number)+" °"+self.FromScale+" have been converted into "+str(r)+" °"+self.ToScale)
 
     def convertKtoF(self):
-        r = (self.FromScale-273.15)*9/5 + 32
-        return r
+        r = (self.number-273.15)*9/5 + 32
+        return print(str(self.number)+" °"+self.FromScale+" have been converted into "+str(r)+" °"+self.ToScale)
     # End - From K
