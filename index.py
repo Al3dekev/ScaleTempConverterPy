b50140ed68c23c2af988061ac1c2745cc477b8fb
Al3dekev <quentin1998@me.com>
2019-07-31 18:34:31 +0200

    Finishing for loop of conversion

--- index.py
+++ index.py
@@ -5,8 +5,9 @@ from FahrenheitScale import *
 
 from UserCallConverter import *
 from UniversalConverter import UniversalConverter
+from ScaleUniversalDatas import *
 
-scaleNumberInProg = 2
+#scaleNumberInProg = 2
 
 Beg = Beginning()
 Beg.welcome()
@@ -18,8 +19,34 @@ UCC.selectNumToConvert()
 UCC.selectScaleFrom()
 UCC.selectScaleTo()
 
-x = 0
+UC = UniversalConverter()
 
-while(x <= scaleNumberInProg):
-    x += 1
+for el in scaleList:
+    # if el.get("ScaleSymbol") == UCC.getSelScaleF():
+    #     for ele in scaleList:
+    #         if ele.get("ScaleSymbol") == UCC.getselScaleT():
 
+    # From
+    if el.get("ScaleSymbol") == "C":
+        for ele in scaleList:
+            # To
+            if ele.get("ScaleSymbol") == "F":
+                UC.convertCtoF()
+            if ele.get("ScaleSymbol") == "K":
+                UC.convertCtoK()
+    if el.get("ScaleSymbol") == "F":
+        for ele in scaleList:
+            # To
+            if ele.get("ScaleSymbol") == "C":
+                UC.convertFtoC()
+            if ele.get("ScaleSymbol") == "K":
+                UC.convertFtoK()
+    if el.get("ScaleSymbol") == "K":
+        for ele in scaleList:
+            # To
+            if ele.get("ScaleSymbol") == "C":
+                UC.convertKtoC()
+            if ele.get("ScaleSymbol") == "F":
+                UC.convertKtoF()
+
+# End for loop
