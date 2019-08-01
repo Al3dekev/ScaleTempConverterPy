bf47cd376652f523d41faf36755641d7e58ca7cd
Al3dekev <quentin1998@me.com>
2019-08-01 13:47:06 +0200

    Manage language expressions of the degree symbol and degree symbol removed for Kelvin

--- UniversalConverter.py
+++ UniversalConverter.py
@@ -15,7 +15,7 @@ class UniversalConverter:
 
     def convertCtoK(self):
         r = self.number + 273.15
-        return print(str(self.number)+" °"+self.FromScale+" have been converted into "+str(r)+" °"+self.ToScale)
+        return print(str(self.number)+" °"+self.FromScale+" have been converted into "+str(r)+""+self.ToScale)
     # End - From C
 
     # From F
@@ -25,15 +25,15 @@ class UniversalConverter:
 
     def convertFtoK(self):
         r = (self.number-32)*5/9 + 273.15
-        return print(str(self.number)+" °"+self.FromScale+" have been converted into "+str(r)+" °"+self.ToScale)
+        return print(str(self.number)+" °"+self.FromScale+" have been converted into "+str(r)+""+self.ToScale)
     # End - From F
 
     # From K
     def convertKtoC(self):
         r = self.number - 273.15
-        return print(str(self.number)+" °"+self.FromScale+" have been converted into "+str(r)+" °"+self.ToScale)
+        return print(str(self.number)+""+self.FromScale+" have been converted into "+str(r)+" °"+self.ToScale)
 
     def convertKtoF(self):
         r = (self.number-273.15)*9/5 + 32
-        return print(str(self.number)+" °"+self.FromScale+" have been converted into "+str(r)+" °"+self.ToScale)
+        return print(str(self.number)+""+self.FromScale+" have been converted into "+str(r)+" °"+self.ToScale)
     # End - From K
